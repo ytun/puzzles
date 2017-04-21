a37a43f0302b7850d8ca1db09e420a1792ddcfe0
ytun <tunyaminthuzar@gmail.com>
2017-04-20 20:33:33 -0700

    coinChange

--- topics/dynamic/CoinChange.java
+++ topics/dynamic/CoinChange.java
@@ -7,20 +7,77 @@ import java.util.Arrays;
 public class CoinChange{
 
 	// number of ways
-	public int coinChange(int[] coins, int x){
+	public static int coinChange(int[] coins, int nCoins, int x){
 		
-		if(x<=0 || coins.length==0){
-			
+		if(x<0 || (nCoins<=0 && x>0)){
+			return 0;
+		}
+		else if(x==0 ){
+			return 1;
+		}
+		else{
+			return coinChange(coins, nCoins, x - coins[nCoins-1]) + coinChange(coins, nCoins -1, x);
 		}
 
 	}
 	
+	public static int coinChangeLoop(int[] coins, int x){
+
+		int[][] table = new int[coins.length][x];
+
+		for(int i=0; i<x; i++){
+			table[0][i] = 1; //one way if amount is 0- no coins put
+		}
+
+		for(int c=0; c<table.length; c++){
+			for(int i=1; i<=x; i++){
+
+				table[c][i] = table[c-1][i] + table[c][i - coins[c-1]];
+
+			}
+		}
+
+		return table[coins.length-1][x];
+	}
+
+	static long countWays(int S[], int m, int n)
+    {
+        //Time complexity of this function: O(mn)
+        //Space Complexity of this function: O(n)
+ 
+        // table[i] will be storing the number of solutions
+        // for value i. We need n+1 rows as the table is
+        // constructed in bottom up manner using the base
+        // case (n = 0)
+        long[] table = new long[n+1];
+ 
+        // Initialize all table values as 0
+        Arrays.fill(table, 0);   //O(n)
+ 
+        // Base case (If given value is 0)
+        table[0] = 1;
+ 
+        // Pick all coins one by one and update the table[]
+        // values after the index greater than or equal to
+        // the value of the picked coin
+        for (int i=0; i<m; i++){
+            for (int j=S[i]; j<=n; j++){
+                table[j] += table[j-S[i]];
+            	System.out.println(i+", "+j+" "+Arrays.toString(table));
+            }
+        }
+ 
+
+        return table[n];
+    }
 
 	public static void main(String[] args){
 		
-		int[] coins = new int[]{1,2,5};
-		int x = 5;
-		System.out.println(coinChange(coins, x));
+		int[] coins = new int[]{1,2,3};
+		int x = 3;
+		// System.out.println(coinChange(coins, coins.length, x));
+		System.out.println(countWays(coins, coins.length,x));
+
 	}
 
 	
